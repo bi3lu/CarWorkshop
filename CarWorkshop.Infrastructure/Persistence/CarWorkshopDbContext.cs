33ed067c666368c8ec26131ee849db48f8ceff5c
Kuba Bielecki <kuba.bielecki@outlook.com>
2023-07-26 09:48:11 +0200

    Added EntityFramework.
    Added EntityFrameworkCore.SqlServer and EntityFrameworkCore.Tools to Infrastructure project.

---
+++ CarWorkshop.Infrastructure/Persistence/CarWorkshopDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CarWorkshop.Infrastructure.Persistence
+{
+    public class CarWorkshopDbContext : DbContext
+    {
+        private readonly string _connectionString = "Server=(localdb)\\mssqllocaldb;Database=CarWorkshopDb;Trusted_Connection=True;";
+
+        public DbSet<Domain.Entities.CarWorkshop> CarWorkshops { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Domain.Entities.CarWorkshop>()
+                .OwnsOne(c => c.ContactDetails);
+        }
+    }
+}
