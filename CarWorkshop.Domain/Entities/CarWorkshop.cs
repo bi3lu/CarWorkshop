9892b149526f593836222ab062743b0a50fa8a78
Kuba Bielecki <kuba.bielecki@outlook.com>
2023-07-26 10:30:54 +0200

    CarWorkshop new migration.
    Aplied new migration - added new prop. to CarWorkshop entity.

--- CarWorkshop.Domain/Entities/CarWorkshop.cs
+++ CarWorkshop.Domain/Entities/CarWorkshop.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; } = default!;
         public string? Description { get; set; }
+        public string? About { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public CarWorkshopContactDetails ContactDetails { get; set; } = default!;
 
