e3aef962957e2d0070983c0bf5d953e744f310ca
Kuba Bielecki <kuba.bielecki@outlook.com>
2023-08-11 10:05:48 +0200

    Added reusable component for details view.

--- CarWorkshop.MVC/wwwroot/js/CarWorkshop/Edit.js
+++ CarWorkshop.MVC/wwwroot/js/CarWorkshop/Edit.js
@@ -1,67 +1,5 @@
 ﻿$(document).ready(function () {
 
-    const RenderCarWorkshopServices = (services, container) => {
-
-        container.empty();
-
-        for (const service of services) {
-
-            container.append(
-
-                `<div class="card border-secondary mb-3" style="max-width: 18rem;">
-
-<div class="card-header">${service.cost}</div>
-
-<div class="card-body">
-
-<h5 class="card-title">${service.description}</h5>
-
-</div>
-
-</div>`)
-
-        }
-
-    }
-
-    const LoadCarWorkshopServices = () => {
-
-        const container = $("#services");
-
-        const carWorkshopEncodedName = container.data("encodedName");
-
-
-
-        $.ajax({
-
-            url: `/CarWorkshop/${carWorkshopEncodedName}/CarWorkshopService`,
-
-            type: 'get',
-
-            success: function (data) {
-
-                if (!data.length) {
-
-                    container.html("There ara no services for this workshop")
-
-                } else {
-
-                    RenderCarWorkshopServices(data, container)
-
-                }
-
-            },
-
-            error: function () {
-
-                toastr["error"]("Something went wrong")
-
-            }
-
-        })
-
-    }
-
     LoadCarWorkshopServices()
 
     $("#createCarWorkshopServiceModal form").submit(function (event) {
@@ -81,7 +19,7 @@
             success: function (data) {
 
                 toastr["success"]("Created carworkshop service")
-
+                LoadCarWorkshopServices()
             },
 
             error: function () {
--- CarWorkshop.MVC/wwwroot/js/site.js
+++ CarWorkshop.MVC/wwwroot/js/site.js
@@ -1,4 +1,43 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿const RenderCarWorkshopServices = (services, container) => {
 
-// Write your JavaScript code.
+    container.empty();
+
+    for (const service of services) {
+        container.append(
+            `<div class="card border-secondary mb-3" style="max-width: 18rem;">
+
+<div class="card-header">${service.cost}</div>
+
+<div class="card-body">
+
+<h5 class="card-title">${service.description}</h5>
+
+</div>
+
+</div>`)
+    }
+}
+
+const LoadCarWorkshopServices = () => {
+
+    const container = $("#services");
+
+    const carWorkshopEncodedName = container.data("encodedName");
+
+    $.ajax({
+
+        url: `/CarWorkshop/${carWorkshopEncodedName}/CarWorkshopService`,
+        type: 'get',
+        success: function (data) {
+
+            if (!data.length) {
+                container.html("There ara no services for this workshop")
+            } else {
+                RenderCarWorkshopServices(data, container)
+            }
+        },
+        error: function () {
+            toastr["error"]("Something went wrong")
+         }
+    })
+}
