61f03f7ba22a7ab923362b11812e0e8ea16cf7c1
Kuba Bielecki <kuba.bielecki@outlook.com>
2023-08-12 11:02:54 +0200

    Added unit tests for CarWorkshop.Domain.

---
+++ CarWorkshop.DomainTests/Entities/CarWorkshopTests.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace CarWorkshop.Domain.Entities.Tests
+{
+    public class CarWorkshopTests
+    {
+        [Fact()]
+        public void EncodeName_ShouldSetEncodedName()
+        {
+            // arrange
+            var carWorkshop = new CarWorkshop();
+            carWorkshop.Name = "Test Workshop";
+
+            // act
+            carWorkshop.EncodeName();
+
+            // assert
+            carWorkshop.EncodedName.Should().Be("test-workshop");
+        }
+
+        [Fact()]
+        public void EncodedName_ShouldThrowException_WhenNameIsNull()
+        {
+            // arrange
+            var carWorkshop = new CarWorkshop();
+
+            // act
+            Action action = () => carWorkshop.EncodeName();
+
+            // assert
+            action.Invoking(a => a.Invoke())
+                .Should().Throw<NullReferenceException>();
+        }
+    }
+}
