6fbb1ca8ec63e879aedf3a51f592b2506d177217
Kuba Bielecki <kuba.bielecki@outlook.com>
2023-08-12 12:30:10 +0200

    Added HomeController test.

---
+++ CarWorkshop.MVCTests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System.Net;
+using Xunit;
+
+namespace CarWorkshop.MVC.Controllers.Tests
+{
+    public class HomeControllerTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+
+        public HomeControllerTests(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact()]
+        public async Task About_ReturnsViewWithRenderModel()
+        {
+            // arrange
+            var client = _factory.CreateClient();
+
+            // act
+            var respone = await client.GetAsync("/Home/About");
+
+            // assert
+            respone.StatusCode.Should().Be(HttpStatusCode.OK);
+
+            var content = await respone.Content.ReadAsStringAsync();
+
+            content.Should().Contain("<div class =\"alert alert-primary\">Some description...</div>");
+        }
+    }
+}
