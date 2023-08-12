f0ca9f1430b694a9d8b424efa0b11131a9eed7b2
Kuba Bielecki <kuba.bielecki@outlook.com>
2023-08-12 11:09:44 +0200

    Added tests for CarWorkshop.Application.

---
+++ CarWorkshop.ApplicationTests/ApplicationUser/CurrentUserTests.cs
@@ -0,0 +1,47 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace CarWorkshop.Application.ApplicationUser.Tests
+{
+    public class CurrentUserTests
+    {
+        [Fact()]
+        public void IsInRole_WithMatchingRole_ShouldReturnTrue()
+        {
+            // arrange
+            var currentUser = new CurrentUser("1", "test@test.com", new List<string> { "Admin", "User" });
+
+            // act
+            var isInRole = currentUser.IsInRole("Admin");
+
+            // assert
+            isInRole.Should().BeTrue();
+        }
+
+        [Fact()]
+        public void IsInRole_WithNonMatchingRole_ShouldReturnFalse()
+        {
+            // arrange
+            var currentUser = new CurrentUser("1", "test@test.com", new List<string> { "Admin", "User" });
+
+            // act
+            var isInRole = currentUser.IsInRole("Manager");
+
+            // assert
+            isInRole.Should().BeFalse();
+        }
+
+        [Fact()]
+        public void IsInRole_WithNonMatchingCaseRole_ShouldReturnFalse()
+        {
+            // arrange
+            var currentUser = new CurrentUser("1", "test@test.com", new List<string> { "Admin", "User" });
+
+            // act
+            var isInRole = currentUser.IsInRole("admin");
+
+            // assert
+            isInRole.Should().BeFalse();
+        }
+    }
+}
